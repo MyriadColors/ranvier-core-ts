e75ae32fe91567d378d8d53f851737ede9b7d352
MyriadColors <pedrotorresgamedesign@gmail.com>
2026-06-09 10:17:28 -0300

    refactor: remove any from Util.ts

--- src/Util.ts
+++ src/Util.ts
@@ -1,12 +1,18 @@
 import { EventEmitter } from 'events';
 
+/**
+ * Standard constructor type
+ */
+// TODO: [Ref] Define specific type for constructor arguments if possible, though any[] is standard for mixins
 export type Constructor<T = EventEmitter> = new (...args: any[]) => T;
 
 /**
  * Check to see if a given object is iterable
- * @param {unknown} obj
- * @return {boolean}
  */
 export function isIterable(obj: unknown): obj is Iterable<unknown> {
-	return !!(obj && typeof (obj as any)[Symbol.iterator] === 'function');
+  return (
+    obj !== null &&
+    obj !== undefined &&
+    typeof (obj as Iterable<unknown>)[Symbol.iterator] === 'function'
+  );
 }
--- test/unit/Util.test.ts
+++ test/unit/Util.test.ts
@@ -1,42 +1,26 @@
-import { describe, it, expect } from "vitest";
-import { isIterable } from "../../src/Util";
+import { describe, it, expect } from 'vitest';
+import { isIterable } from '../../src/Util';
 
-describe("Util", () => {
-	describe("isIterable", () => {
-		it("should return true for arrays", () => {
-			expect(isIterable([])).toBe(true);
-			expect(isIterable([1, 2, 3])).toBe(true);
-		});
-
-		it("should return true for maps", () => {
-			expect(isIterable(new Map())).toBe(true);
-		});
-
-		it("should return true for sets", () => {
-			expect(isIterable(new Set())).toBe(true);
-		});
-
-		it("should return true for strings", () => {
-			expect(isIterable("test")).toBe(true);
-		});
-
-		it("should return false for plain objects", () => {
-			// @ts-expect-error
-			expect(isIterable({})).toBe(false);
-		});
-
-		it("should return false for null/undefined", () => {
-			// @ts-expect-error
-			expect(isIterable(null)).toBe(false);
-			// @ts-expect-error
-			expect(isIterable(undefined)).toBe(false);
-		});
-
-		it("should return false for numbers/booleans", () => {
-			// @ts-expect-error
-			expect(isIterable(123)).toBe(false);
-			// @ts-expect-error
-			expect(isIterable(true)).toBe(false);
-		});
-	});
+describe('Util', () => {
+  describe('isIterable', () => {
+    it('returns true for arrays', () => {
+      expect(isIterable([])).toBe(true);
+    });
+    it('returns true for strings', () => {
+      expect(isIterable('test')).toBe(true);
+    });
+    it('returns true for Map', () => {
+      expect(isIterable(new Map())).toBe(true);
+    });
+    it('returns false for objects', () => {
+      // @ts-expect-error
+      expect(isIterable({})).toBe(false);
+    });
+    it('returns false for null/undefined', () => {
+      // @ts-expect-error
+      expect(isIterable(null)).toBe(false);
+      // @ts-expect-error
+      expect(isIterable(undefined)).toBe(false);
+    });
+  });
 });
